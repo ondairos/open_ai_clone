12168e6a2cecafe8a681ff4638c4ffe53c6ad9fa
Ondairos <ondairos@gmail.com>
2023-02-13 14:19:45 +0200

    css changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
   return (
     <div className='footer-div'>
-        <h2>Ioannis Kantiloros 2023.</h2>
+        <h2>Ioannis Kantiloros 2023. Using OpenAI API</h2>
     </div>
   )
 }
